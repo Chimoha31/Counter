19aaf73143b1bc97c0aff48dcc0dc4c4eb3b45d3
Chimoha31 <mokochii1108@gmail.com>
2022-04-19 20:34:42 -0700

    Done

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -4,21 +4,27 @@ const initialState = {
   count: 0,
 }
 
-// 第一引数に名前、第二引数にinitialState、その後にinitialStateを変更していくからreducerを第三引数にとる。
+// 第一引数に名前、第二引数にinitialState、その後にinitialStateを変更していくからreducersを第三引数にとる。
 export const counterSlice = createSlice({
   name: "counter",
   initialState,
-  reducer: {
+  reducers: {
     increment: (state) => {
       state.count += 1;
     },
     decrement: (state) => {
       state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload
     }
   }
 });
 
 // storeの中のreducerに持ってくまでにactionsが必要＝{increment, decrementr}をactionsによってstoreに渡す。
-export const {increment, decrementr} = counterSlice.actions;
+export const {increment, decrement, reset, incrementByAmount} = counterSlice.actions;
 // reducer＝変える。storeに中にあるreducer＝{increment, decrementr}は変更される値だから。
 export default counterSlice.reducer
